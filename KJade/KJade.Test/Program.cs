d56cc2864865eeae1efda67056f28ff439ec0ebe
0xFireball <0xFireball@outlook.com>
2016-10-03 11:51:00 -0700

    create class structure

--- KJade/KJade.Test/Program.cs
+++ KJade/KJade.Test/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace KJade.Test
+﻿using KJade.Compiler.Html;
+
+namespace KJade.Test
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var compiler = new JadeHtmlCompiler();
         }
     }
 }
---
+++ KJade/src/KJade/Compiler/Html/JadeHtmlCompiler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KJade.Compiler.Html
+{
+    public class JadeHtmlCompiler : IJadeCompiler
+    {
+        public IJadeCompileResult Compile(TextReader input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
