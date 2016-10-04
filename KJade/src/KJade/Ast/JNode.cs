7cada8252754c3ea6afad7087126c8d7861f70b3
0xFireball <0xFireball@outlook.com>
2016-10-04 13:55:12 -0700

    add node classes to Ast

---
+++ KJade/src/KJade/Ast/JNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace KJade.Ast
+{
+    public abstract class JNode
+    {
+        public List<JNode> Children { get; set; } = new List<JNode>();
+
+        public StringBuilder TextRepresentation { get; set; } = new StringBuilder();
+    }
+}
---
+++ KJade/src/KJade/Ast/JRootNode.cs
@@ -0,0 +1,9 @@
+﻿namespace KJade.Ast
+{
+    /// <summary>
+    /// A node representing a root node.
+    /// </summary>
+    public class JRootNode : JNode
+    {
+    }
+}
