bdfda857bdbe112b237a8fc66ab8e4ec0e8c2441
0xFireball <0xFireball@outlook.com>
2016-10-07 14:23:21 -0700

    have an option to allow for nonexistent model properties

--- KJade/src/KJade.ViewEngine/KJadeViewEngine.cs
+++ KJade/src/KJade.ViewEngine/KJadeViewEngine.cs
@@ -33,7 +33,7 @@ namespace KJade.ViewEngine
 
         private static readonly Regex ImportRegex = new Regex(@"@import\s(?<ViewName>(\w|/)+)", RegexOptions.Compiled);
 
-        private static readonly Regex ConditionalRegex = new Regex(@"@if\smodel(?:\.(?<ParameterName>[a-zA-Z0-9-_]+)+)?(?<Contents>[\s\S]*?)@endif", RegexOptions.Compiled);
+        private static readonly Regex ConditionalRegex = new Regex(@"@if(?<AllowNonexistent>\?)?\smodel(?:\.(?<ParameterName>[a-zA-Z0-9-_]+)+)?(?<Contents>[\s\S]*?)@endif", RegexOptions.Compiled);
 
         public Response RenderView(ViewLocationResult viewLocationResult, dynamic model, IRenderContext renderContext)
         {
@@ -80,13 +80,14 @@ namespace KJade.ViewEngine
             {
                 var properties = ModelReflectionUtil.GetCaptureGroupValues(m, "ParameterName");
                 var propertyVal = ModelReflectionUtil.GetPropertyValueFromParameterCollection(model, properties);
+                var allowNonexistent = m.Groups["AllowNonexistent"].Success;
 
-                if (!propertyVal.Item1)
+                if (!propertyVal.Item1 && !allowNonexistent)
                 {
                     return "[ERR!]";
                 }
 
-                bool evaluateResult = propertyVal != null;
+                bool evaluateResult = propertyVal.Item2 != null;
 
                 var conditionalContent = m.Groups["Contents"].Value;
 
